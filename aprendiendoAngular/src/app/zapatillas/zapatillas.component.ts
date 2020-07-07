b8c7e9a57baa844d0cc9125ef44134ba6932edda
Julio <julio_047@hotmail.com>
2020-07-06 19:49:53 -0500

    directivas ngswitch y ngstyle

--- aprendiendoAngular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendoAngular/src/app/zapatillas/zapatillas.component.ts
@@ -10,13 +10,29 @@ export class ZapatillasComponent implements OnInit{
     public titulo = "Componente de Zapatillas";    
     public muchosTenis: Array<Tenis>;
 
+    public colores: String[];
+
+    public color: string;
+
     constructor(){    
         this.muchosTenis = [
             new Tenis("Nike","Blancos",1600,false),
             new Tenis("Adidas","Blancos",800,true),
             new Tenis("Puma","Rosa",499,false),
-            new Tenis("Charlie","Negro",350,true)
-        ]; 
+            new Tenis("Charlie","Negro",350,true)            
+        ];         
+
+        //Guardamos en un array los colores de los Tenis, si ya existe ese color no lo guarda y al contrario.
+        this.colores = new Array();
+        this.muchosTenis.forEach( (tenis, indice) =>{
+            if( this.colores.indexOf(tenis.color) < 0){
+                this.colores.push(tenis.color);
+                console.log(indice);
+            }            
+        });
+
+        this.color = 'yellow';
+
     }    
 
     ngOnInit(){
